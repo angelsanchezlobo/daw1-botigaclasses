931b346bdbcecbabd8185dd7e3db3a28b12a5f9d
Alex Caner <a2308752@institutmontilivi.cat>
2024-03-07 11:18:18 +0100

    Primer commit

---
+++ ClasseBotiga/ClasseBotiga/Cistella.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseBotiga
+{
+    internal class Cistella
+    {
+        // Atributs
+        private Botiga botiga;
+        private DateTime data;
+        private Producte[] productes;
+        private int nElements;
+        private double diners;
+
+        // Propietats
+
+        // Constructors
+        public Cistella()
+        {
+            botiga = null;
+            data = "";
+            productes = 10;
+            nElements = 0;
+            diners = 0;
+        }
+        public Cistella(Producte productes, int quantitat)
+        {
+            this.botiga = botiga;
+            this.productes[nElements] = productes;
+            nElements = this.productes.Length;
+
+            // Caluclar si hi han diners suficients
+
+        }
+
+        // Propietats
+        public Producte Productes
+        {
+            get { return Productes; }
+            set {  }
+        }
+        public int NElements
+        {
+            get { return nElements; }
+        }
+        public double Diners
+        {
+            get { return diners; }
+            set { diners += value; }
+        }
+
+        // Metodos
+    }
+}
