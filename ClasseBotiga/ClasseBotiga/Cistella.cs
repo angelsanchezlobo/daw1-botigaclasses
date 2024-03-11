06fa33d515267a34e1368fc3b53e0222c80430f6
Alex Caner <a2308752@institutmontilivi.cat>
2024-03-11 12:29:12 +0100

    Cistella acabada correctament

--- ClasseBotiga/ClasseBotiga/Cistella.cs
+++ ClasseBotiga/ClasseBotiga/Cistella.cs
@@ -1,211 +1,157 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClasseBotiga
 {
     internal class Cistella
     {
-        // Atributs
+        // Atributos
         private Botiga botiga;
         private DateTime data;
         private Producte[] productes;
         private int nElements;
         private double diners;
 
-        // Constructors
+        // Constructores
         public Cistella()
         {
             botiga = null;
-            data = "";
-            productes = 10;
+            data = DateTime.Now;
+            productes = new Producte[10];
             nElements = 0;
             diners = 0;
         }
-        public Cistella(Producte productes, int quantitat)
+
+        public Cistella(Producte[] productes, int quantitat)
         {
-            if(diners - CostTotal() < 0)
+            if (diners - CostTotal() < 0)
             {
-                Console.WriteLine("No hi han prous diners");
+                Console.WriteLine("No hi ha prous diners");
             }
-            else if (nElements > productes.Length)
+            else if (nElements >= productes.Length)
             {
-                string resposta;
                 Console.WriteLine("No hi ha espai, vols afegir?");
-                resposta = Console.ReadLine();
+                string resposta = Console.ReadLine();
                 if (resposta == "si" || resposta == "Si")
                 {
-                    productes aux = new productes[productes.GetLength + 1];
-                    for (int i = 0; i < productes.GetLength(); i++)
+                    Producte[] aux = new Producte[productes.Length + 1];
+                    for (int i = 0; i < productes.Length; i++)
                         aux[i] = productes[i];
                     productes = aux;
                 }
             }
             else
             {
-                this.botiga = botiga;
-                this.productes[nElements] = productes;
+                this.productes[nElements] = productes[nElements];
                 nElements = this.productes.Length;
             }
-
-
         }
 
-        // Propietats
-        public Producte Productes
+        // Propiedades
+        public Producte[] Productes
         {
-            get { return Productes; }
-            set
-            {
-                if(diners - CostTotal()< 0) 
-                {
-                    Console.WriteLine("No hi han prous diners");
-                }
-                else if(nElements > productes.Length)
-                {
-                    string resposta;
-                    Console.WriteLine("No hi ha espai, vols afegir?");
-                    resposta = Console.ReadLine();
-                    if (resposta == "si" || resposta == "Si")
-                    {
-                        productes aux = new productes[productes.GetLength + 1];
-                        for (int i = 0; i < productes.GetLength(); i++)
-                            aux[i] = productes[i];
-                        productes = aux;
-                    }
-                }
-                else
-                {
-                    productes[nElements].Preu_sense_iva = value;
-                    productes[nElements].Iva = value;
-                    productes[nElements].Quantitat = value;
-                }
-            }
+            get { return productes; }
+            set { productes = value; }
         }
+
         public int NElements
         {
             get { return nElements; }
         }
+
         public double Diners
         {
             get { return diners; }
             set { diners += value; }
         }
+
         public DateTime Data
         {
             get { return data; }
         }
 
-        // Metodos
-        /// <summary>
-        /// Afegeix al array de productes un producte en base al objecte producte que se l'hi passa
-        /// </summary>
-        /// <param name="producte"> És el objecte producte que s'ha de afegir al array de productes de la cistella</param>
+        // Métodos
         public void ComprarProducte(Producte producte)
         {
-            bool existeix = false;
-             
-            if (nElements > productes.Length)
+            if (nElements >= productes.Length)
             {
-                string resposta;
                 Console.WriteLine("No hi ha espai, vols afegir?");
-                resposta = Console.ReadLine();
-                if (resposta == "si" || resposta == "Si")
-                {
-                    productes aux = new productes[productes.GetLength + 1];
-                    for (int i = 0; i < productes.GetLength(); i++)
-                        aux[i] = productes[i];
-                    productes = aux;
-                }
-                Producte.data = DateTime.now();
-            }
-           else if(diners - CostTotal() > 0)
-            {
-                Console.WriteLine("No hi han prous diners, vols afegir?");
                 string resposta = Console.ReadLine();
                 if (resposta == "si" || resposta == "Si")
                 {
-                    Console.WriteLine("Cuants vols afegir?");
-                    double afegir = Convert.ToDouble(Conosle.ReadLine());
-                    Producte.Diners(afegir);
+                    Array.Resize(ref productes, productes.Length + 1);
+                }
+            }
+            else if (diners - CostTotal() > 0)
+            {
+                Console.WriteLine("No hi ha prous diners, vols afegir?");
+                string resposta = Console.ReadLine();
+                if (resposta == "si" || resposta == "Si")
+                {
+                    Console.WriteLine("Quants vols afegir?");
+                    double afegir = Convert.ToDouble(Console.ReadLine());
+                    Diners = afegir;
                 }
             }
             else
             {
-                productes[nElements].nom = producte.Nom;
-                productes[nElements].preu_sense_iva = producte.Preu_sense_iva;
-                productes[nElements].iva = producte.Iva;
-                productes[nElements].quantitat = producte.Quantitat;
+                productes[nElements].Nom = producte.Nom;
+                productes[nElements].Preu_sense_iva = producte.Preu_sense_iva;
+                productes[nElements].Iva = producte.Iva;
+                productes[nElements].Quantitat = producte.Quantitat;
                 nElements++;
             }
         }
-        /// <summary>
-        /// Ordena el array de productes per nom amb bubble sort
-        /// </summary>
+
         public void OrdenarCistella()
         {
-            int n = productes.Length;
-            for (int i = 0; i < n - 1; i++)
+            for (int i = 0; i < nElements - 1; i++)
             {
-                for (int j = 0; j < n - i - 1; j++)
+                for (int j = 0; j < nElements - i - 1; j++)
                 {
-                    if (productes[j].nom > productes[j + 1].nom)
+                    if (productes[j].Nom.CompareTo(productes[j + 1].Nom) > 0)
                     {
-                        int temp = productes[j].nom;
-                        productes[j].nom = productes[j + 1].nom;
-                        productes[j + 1].nom = temp;
+                        Producte temp = productes[j];
+                        productes[j] = productes[j + 1];
+                        productes[j + 1] = temp;
                     }
                 }
             }
         }
-        /// <summary>
-        /// Imprimeix el array de productes de manera amigable, mostrant preu unitari, amb impost...
-        /// </summary>
+
         public void Mostrar()
         {
-            for(int i = 0;i < NElements;i++)
+            for (int i = 0; i < NElements; i++)
             {
-                if (productes[i].Nom != null)
-                {
-                    Console.Write($"Producte: {productes[i].nom}    ");
-                    Console.Write($"Sense IVA: {productes[i].preu_sense_iva}    ");
-                    Console.Write($"IVA: {productes[i].iva}    ");
-                    Console.Write($"Preu unitari: {productes[i].preu_sense_iva + productes[i].iva}    ");
-                    Console.Write($"Quantitat: {productes[i].quantitat}     ");
-                    Console.Write($"Total: {productes[i].quantitat * (productes[i].preu_sense_iva + productes[i].iva)}     ");
-                }
+                Console.WriteLine($"Producte: {productes[i].Nom}");
+                Console.WriteLine($"Sense IVA: {productes[i].Preu_sense_iva}");
+                Console.WriteLine($"IVA: {productes[i].Iva}");
+                Console.WriteLine($"Preu unitari: {productes[i].Preu_sense_iva + productes[i].Iva}");
+                Console.WriteLine($"Quantitat: {productes[i].Quantitat}");
+                Console.WriteLine($"Total: {productes[i].Quantitat * (productes[i].Preu_sense_iva + productes[i].Iva)}");
             }
-            Console.Write($"TOTAL: {CostTotal()}");
+            Console.WriteLine($"TOTAL: {CostTotal()}");
         }
-        /// <summary>
-        /// Calcula el preu de tot el que hi ha a la cistella amb iva inclós
-        /// </summary>
-        /// <returns> Total es el total de diners que costa la cistella </returns>
+
         public double CostTotal()
         {
             double total = 0;
             for (int i = 0; i < nElements; i++)
             {
-                double ivaCalculat = productes[i].Preu_sense_iva / productes[i].Iva;
-                total = (productes[i].Preu_sense_iva + ivaCalculat) * productes[i].Quantitat;
+                double ivaCalculat = productes[i].Preu_sense_iva * productes[i].Iva;
+                total += (productes[i].Preu_sense_iva + ivaCalculat) * productes[i].Quantitat;
             }
             return total;
         }
-        /// <summary>
-        /// Mostra el nom del producte y el total de tot amb IVA inclós
-        /// </summary>
-        public void ToString()
+
+        public override string ToString()
         {
+            string result = "";
             for (int i = 0; i < NElements; i++)
             {
-                if (productes[i].Nom != null)
-                {
-                    Console.Write($"Producte: {productes[i].nom}    ");
-                }
+                result += $"Producte: {productes[i].Nom}\n";
             }
-            Console.Write($"TOTAL: {CostTotal()}");
+            result += $"TOTAL: {CostTotal()}";
+            return result;
         }
     }
 }
