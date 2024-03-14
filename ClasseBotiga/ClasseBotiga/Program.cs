dea51806e01047e8a56c7eab4a8e6dbac75e1efe
Alex Caner <a2308752@institutmontilivi.cat>
2024-03-14 10:37:14 +0100

    Commit main començat a crear

--- ClasseBotiga/ClasseBotiga/Program.cs
+++ ClasseBotiga/ClasseBotiga/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            int opcio;
+            Botiga[] botigues = new Botiga[20];
+            int nElemB = 0;
+            Producte[] productes = new Producte[20];
+            int nElemP = 0;
+            Cistella[] cistellaProd = new Cistella[20];
+            int nElemC = 0;
+            int opcio = 0;
             do
             {
             Console.Clear();
@@ -22,7 +28,7 @@
             {
                 case 1:
                     Console.Clear();
-                    int opcio1;
+                    int opcio1 = 0;
                     Console.WriteLine("1. Botiga ");
                     Console.WriteLine("2. Producte\n");
                     Console.WriteLine("0. Sortir\n");
@@ -35,7 +41,7 @@
                     switch (opcio1)
                         {
                             case 1:
-                                int opcio2;
+                                int opcio2 = 0;
                                 do
                                 {
                                     Console.Clear();
@@ -59,7 +65,8 @@
                                     switch (opcio2)
                                     {
                                         case 1:
-                                            // Crear botiga
+                                            Console.Clear();
+                                            CrearBotiga(ref nElemB, botigues);
                                             break;
                                         case 2:
                                             // Afegir Producte
@@ -74,13 +81,22 @@
                                             // Modificar Producte
                                             break;
                                         case 6:
-                                            // Ordenar Productes
+                                            Console.WriteLine("Quina botiga vols ordenar");
+                                            string nomB = Console.ReadLine();
+                                            int pos = TrobarBotiga(botigues, nElemB, nomB);
+                                            botigues[pos].OrdenarProducte();
                                             break;
                                         case 7:
-                                            // Ordenar Preu
+                                            Console.WriteLine("Quina botiga vols ordenar");
+                                            nomB = Console.ReadLine();
+                                            pos = TrobarBotiga(botigues, nElemB, nomB);
+                                            botigues[pos].OrdenarPreus();
                                             break;
                                         case 8:
-                                            // Mostrar
+                                            Console.WriteLine("Quina botiga vols mostrar");
+                                            nomB = Console.ReadLine();
+                                            pos = TrobarBotiga(botigues, nElemB, nomB);
+                                            botigues[pos].Mostrar();
                                             break;
                                         case 0:
                                             // Sortir
@@ -89,19 +105,18 @@
                                 } while (opcio2 != 0);
                                 break;
                             case 2:
-                                int opcio3;
+                                int opcio3 = 0;
                                 do
                                 {
                                     Console.Clear();
                                     Console.WriteLine("       PRODUCTES      \n");
                                     Console.WriteLine("1. Crear Producte ");
-                                    Console.WriteLine("2. Modificar Producte ");
-                                    Console.WriteLine("3. Consultar Preu");
-                                    Console.WriteLine("4. Mostrar\n");
+                                    Console.WriteLine("2. Consultar Preu");
+                                    Console.WriteLine("3. Mostrar\n");
                                     Console.WriteLine("0. Sortir \n");
                                     Console.Write("Que vols fer: ");
                                     opcio3 = Convert.ToInt32(Console.ReadLine());
-                                    while (opcio3 > 4 || opcio3 < 0)
+                                    while (opcio3 > 3 || opcio3 < 0)
                                     {
                                         Console.Write("INCORRECTE, posa de nou: ");
                                         opcio3 = Convert.ToInt32(Console.ReadLine());
@@ -109,16 +124,24 @@
                                     switch (opcio3)
                                     {
                                         case 1:
-                                            // Crear Producte
+                                            Console.Clear();
+                                            CrearProducte(ref nElemP, productes);
+                                            Console.WriteLine($"Nom producte: {productes[nElemP - 1].Nom}");
+                                            Thread.Sleep(2000);
                                             break;
                                         case 2:
-                                            // Modificar Producte
+                                            // Consultar Preus
+                                            Console.Clear();
+                                            Console.Write("Indica el producte: ");
+                                            string nomP = Console.ReadLine();
+                                            int pos = TrobarProducte(productes, nElemP, nomP);
+                                            Console.WriteLine($"El preu es de {productes[pos].Preu()} Euros");
+                                            Thread.Sleep(4000);
                                             break;
                                         case 3:
-                                            // Consultar Preu
-                                            break;
-                                        case 4:
-                                            // Mostrar
+                                            Console.Clear();
+                                            MostrarProductes(productes, nElemP);
+                                            Thread.Sleep(4000);
                                             break;
                                         case 0:
                                             // Sortir
@@ -130,7 +153,7 @@
                         
                     break;
                 case 2:
-                        int opcio4;
+                        int opcio4 = 0;
                         do
                         {
                             Console.Clear();
@@ -174,6 +197,70 @@
             }
             }
             while(opcio != 0);  
+        }
+        static void CrearBotiga(ref int nElemB, Botiga[] botigues)
+        {
+            //Clear
+            Console.Write("Indica un nom per la nova botiga: ");
+            string nom = Convert.ToString(Console.ReadLine());
+            //Clear
+            Console.WriteLine("Indica el nombre de productes");
+            int nomProductes = Convert.ToInt32(Console.ReadLine());
+            Botiga botiga = new Botiga(nom, nomProductes);
+            botigues[nElemB] = botiga;
+            nElemB++;
+        }
+        static void CrearProducte(ref int nElemP, Producte[] productes)
+        {
+            Console.Write("Indica un nom per el nou producte: ");
+            string nom = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Indica el preu unitari");
+            int preuUnitari = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Indica el iva");
+            int iva = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Indica la quantitat");
+            int quantitat = Convert.ToInt32(Console.ReadLine());
+            Producte producte = new Producte(nom, preuUnitari, iva, quantitat);
+            productes[nElemP] = producte;
+            nElemP++;
+        }
+        static int TrobarProducte(Producte[] productes, int nElemP, string nomP)
+        {
+            bool trobat = false;
+            int posicio = 0;
+            for (int i = 0; i <= nElemP && !trobat; i++)
+            {
+                if (nomP == productes[i].Nom)
+                {
+                    posicio = i;
+                    trobat = true;
+                }
+            }
+            return posicio;
+        }
+        static int TrobarBotiga(Botiga[] botigues, int nElemB, string nomB)
+        {
+            bool trobat = false;
+            int posicio = 0;
+            for (int i = 0; i <= nElemB && !trobat; i++)
+            {
+                if (nomB == botigues[i].NomBotiga)
+                {
+                    posicio = i;
+                    trobat = true;
+                }
+            }
+            return posicio;
+        }
+        static void MostrarProductes(Producte[] productes, int nElemP)
+        {
+            for(int i = 0;i < nElemP; i++)
+            {
+                if (productes[i].Nom != null)
+                {
+                    Console.WriteLine($"Producte: {productes[i].Nom} Stock: {productes[i].Quantitat}");
+                }
+            }
         }
     }
 }
